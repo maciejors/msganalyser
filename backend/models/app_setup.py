b7995fd520a58bef08c93a63d0a5e69a3ea9a71d
maciejors <maciej.orslowski@gmail.com>
2024-02-10 17:12:55 +0100

    add models for app_setup

--- backend/models/app_setup.py
+++ backend/models/app_setup.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 
 class ConfigurationModel(BaseModel):
     data_path: str
+
+
+class DataOwnerModel(BaseModel):
+    data_owner: str
--- backend/routers/app_setup.py
+++ backend/routers/app_setup.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, HTTPException
 
-from backend.models.app_setup import ConfigurationModel
+from backend.models.app_setup import ConfigurationModel, DataOwnerModel
 from backend.services.reading_data.read import (
     read_messenger_data, infer_data_owner,
 )
@@ -13,19 +13,20 @@ router = APIRouter(
 
 
 @router.put('/')
-async def setup(request: Request, config: ConfigurationModel):
+async def setup(request: Request, config: ConfigurationModel) -> DataOwnerModel:
     full_data_df = read_messenger_data(config.data_path, purge_contents=True)
     data_owner = infer_data_owner(full_data_df)
     request.app.state.data_df = full_data_df
     request.app.state.data_owner = data_owner
+    return DataOwnerModel(data_owner=data_owner)
 
 
 @router.get(
     '/', 
-    responses={404: {"description": 'Data is yet to be read'}},
+    responses={404: {'description': 'Data is yet to be read'}},
 )
-async def get_data_owner(request: Request):
+async def get_data_owner(request: Request) -> DataOwnerModel:
     try:
-        return { 'data_owner': request.app.state.data_owner }
+        return DataOwnerModel(data_owner=request.app.state.data_owner)
     except AttributeError:
         raise HTTPException(status_code=404, detail='Data is yet to be read')
