f05148d2fcc5ee355c1eed39df41a91c22b3cf00
maciejors <maciej.orslowski@gmail.com>
2024-02-10 10:00:42 +0100

    create a backend structure

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
+from .routers import chats, activity, app_setup
+
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(app_setup.router)
+app.include_router(activity.router)
+app.include_router(chats.router)
---
+++ backend/routers/chats.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    prefix='/chats',
+    tags=['chats'],
+)
