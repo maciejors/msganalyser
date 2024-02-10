057037fbb5a20b075e32e42772aad5da3271945c
maciejors <maciej.orslowski@gmail.com>
2024-02-10 14:14:25 +0100

    update root path

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from backend.routers import chats, activity, app_setup
 
-app = FastAPI()
+app = FastAPI(root_path='/api')
 app.include_router(app_setup.router)
 app.include_router(activity.router)
 app.include_router(chats.router)
