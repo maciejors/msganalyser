1a3e15a495e048db049c44b347582aea13fae108
maciejors <maciej.orslowski@gmail.com>
2024-02-11 14:15:52 +0100

    rename models, add a model for datetime chat stats

--- backend/models/analysis.py
+++ backend/models/analysis.py
@@ -1,10 +1,23 @@
 from pydantic import BaseModel
 
 
-class DatetimeStat1D(BaseModel):
+class DatetimeActivityStat(BaseModel):
     """
     Represents one-dimenional statistics grouped by datetime or values
-    dependent on datetime (e.g. year, month, day, hour)
+    dependent on datetime (e.g. year, month, day, hour).
+
+    This is used for activity analysis where no chat name is needed.
     """
     keys: list[str | int]
     values: list[int]
+    
+
+class DatetimeChatStat(DatetimeActivityStat):
+    """
+    Represents one-dimenional statistics grouped by datetime or values
+    dependent on datetime (e.g. year, month, day, hour).
+    
+    This is used for chat analysis where datetime is involved, where
+    aside from values a chat name is needed.
+    """
+    chat_names: list[str]
--- backend/routers/activity.py
+++ backend/routers/activity.py
@@ -4,7 +4,7 @@ from backend.dependencies.errors import DataNotReadException
 from backend.dependencies.filters import StandardFiltersAnnotated
 from backend.dependencies.wrappers import functionality_wrapper
 from backend.models.common import BaseAppErrorModel
-from backend.models.analysis import DatetimeStat1D
+from backend.models.analysis import DatetimeActivityStat
 
 from backend.services.activity_analysis.by_year import msg_by_year
 from backend.services.activity_analysis.by_month import msg_by_month, avg_msg_by_calendar_month
@@ -22,51 +22,51 @@ router = APIRouter(
 
 
 @router.get('/msg_by_year')
-async def get_msg_by_year(request: Request, filters: StandardFiltersAnnotated) -> DatetimeStat1D:
+async def get_msg_by_year(request: Request, filters: StandardFiltersAnnotated) -> DatetimeActivityStat:
     try:
         df = request.app.state.data_df
     except AttributeError:
         raise DataNotReadException()
-    response = functionality_wrapper(msg_by_year, df, filters, DatetimeStat1D)
+    response = functionality_wrapper(msg_by_year, df, filters, DatetimeActivityStat)
     return response
 
 
 @router.get('/msg_by_month')
-async def get_msg_by_month(request: Request, filters: StandardFiltersAnnotated) -> DatetimeStat1D:
+async def get_msg_by_month(request: Request, filters: StandardFiltersAnnotated) -> DatetimeActivityStat:
     try:
         df = request.app.state.data_df
     except AttributeError:
         raise DataNotReadException()
-    response = functionality_wrapper(msg_by_month, df, filters, DatetimeStat1D)
+    response = functionality_wrapper(msg_by_month, df, filters, DatetimeActivityStat)
     return response
 
 
 @router.get('/avg_msg_by_calendar_month')
 async def get_avg_msg_by_calendar_month(request: Request,
-                                        filters: StandardFiltersAnnotated) -> DatetimeStat1D:
+                                        filters: StandardFiltersAnnotated) -> DatetimeActivityStat:
     try:
         df = request.app.state.data_df
     except AttributeError:
         raise DataNotReadException()
-    response = functionality_wrapper(avg_msg_by_calendar_month, df, filters, DatetimeStat1D)
+    response = functionality_wrapper(avg_msg_by_calendar_month, df, filters, DatetimeActivityStat)
     return response
 
 
 @router.get('/msg_by_day')
-async def get_msg_by_day(request: Request, filters: StandardFiltersAnnotated) -> DatetimeStat1D:
+async def get_msg_by_day(request: Request, filters: StandardFiltersAnnotated) -> DatetimeActivityStat:
     try:
         df = request.app.state.data_df
     except AttributeError:
         raise DataNotReadException()
-    response = functionality_wrapper(msg_by_day, df, filters, DatetimeStat1D)
+    response = functionality_wrapper(msg_by_day, df, filters, DatetimeActivityStat)
     return response
 
 
 @router.get('/msg_by_time_of_day')
-async def get_msg_by_time_of_day(request: Request, filters: StandardFiltersAnnotated) -> DatetimeStat1D:
+async def get_msg_by_time_of_day(request: Request, filters: StandardFiltersAnnotated) -> DatetimeActivityStat:
     try:
         df = request.app.state.data_df
     except AttributeError:
         raise DataNotReadException()
-    response = functionality_wrapper(msg_by_time_of_day, df, filters, DatetimeStat1D)
+    response = functionality_wrapper(msg_by_time_of_day, df, filters, DatetimeActivityStat)
     return response
