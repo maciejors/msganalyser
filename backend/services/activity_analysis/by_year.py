65ec635dc007b06f3e90fb67d30b85cb064b3e66
maciejors <maciej.orslowski@gmail.com>
2024-02-11 12:53:45 +0100

    reformat code

--- backend/services/activity_analysis/by_year.py
+++ backend/services/activity_analysis/by_year.py
@@ -5,10 +5,10 @@ def msg_per_year(df: pd.DataFrame) -> pd.DataFrame:
     """
     Number of messages per year
     """
-    result = df[['year', 'sender_name']]\
-        .groupby('year')\
-        .count()\
-        .reset_index()\
-        .rename(columns={'year': 'keys', 'sender_name': 'values'})\
+    result = df[['year', 'sender_name']] \
+        .groupby('year') \
+        .count() \
+        .reset_index() \
+        .rename(columns={'year': 'keys', 'sender_name': 'values'}) \
         .sort_values('keys')
     return result
