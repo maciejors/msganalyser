0317815f2582d42c301de18a5b4e104e2df85346
maciejors <maciej.orslowski@gmail.com>
2024-02-15 11:51:49 +0100

    add anonymiser to reading_data/__init__.py

--- backend/services/reading_data/__init__.py
+++ backend/services/reading_data/__init__.py
@@ -1,7 +1,9 @@
 from .read import read_messenger_data, infer_data_owner
+from .anonymiser import anonymise_data
 
 
 __all__ = [
     'read_messenger_data',
     'infer_data_owner',
+    'anonymise_data',
 ]
