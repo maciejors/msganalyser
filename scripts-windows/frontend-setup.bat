78f4210e6027fe2947bd23118090f11b59d991c0
maciejors <maciej.orslowski@gmail.com>
2024-02-15 19:03:51 +0100

    update scripts

--- scripts-windows/frontend-setup.bat
+++ scripts-windows/frontend-setup.bat
@@ -1,3 +1,4 @@
 cd ..\frontend
 npm install
-npm run dev
+npm run build
+node .output\server\index.mjs
--- scripts/frontend-setup.sh
+++ scripts/frontend-setup.sh
@@ -1,5 +1,4 @@
 cd frontend
 npm install
-npm run dev
-# npm run build
-# node .output/server/index.mjs
+npm run build
+node .output/server/index.mjs
